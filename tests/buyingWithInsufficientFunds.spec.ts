e6e7ec019e1c121157fc2d0f242f377b0fda3b63
Nenko Baldzhiev <nenko.baldzhiev@gmail.com>
2024-10-31 13:26:00 +0200

    use test steps

--- tests/buyingWithInsufficientFunds.spec.ts
+++ tests/buyingWithInsufficientFunds.spec.ts
@@ -6,11 +6,18 @@ import { createMetamaskWallet } from './utils/metamaskExtension';
 test('Should not be able to buy NFTs with insufficient funds', async ({ page }) => {
   const password = `Beam-${Date.now()}!`
 
-  // the page seems to load indefinitely, hence the waitUntil: 'domcontentloaded' instead of 'load'
-  await page.goto('/', { waitUntil: 'domcontentloaded' });
+  await test.step('Open the Sphere testnets page', async () => {
+    // the page seems to load indefinitely, hence the waitUntil: 'domcontentloaded' instead of 'load'
+    await page.goto('/', { waitUntil: 'domcontentloaded' });
+  });
 
-  await selectSignInWallet({ page, wallet: 'MetaMask' });
+  await test.step('Select MetaMask as the Sign In wallet', async () => {
+    await selectSignInWallet({ page, wallet: 'MetaMask' });
+  });
 
   const metamaskPage = page.context().pages()[1]
-  await createMetamaskWallet({ page: metamaskPage, password, secureWhen: 'later' })
+
+  await test.step('Create a new MetaMask wallet via the Metamask Chromium extension', async () => {
+    await createMetamaskWallet({ page: metamaskPage, password, secureWhen: 'later' })
+  });
 });
